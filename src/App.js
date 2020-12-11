81b73f29666091c11807fed9efe8a59650b565bc
Choharim <jhl8586@naver.com>
2020-12-11 17:19:01 +0900

    Add increBtn,decreBtn By classComponent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [ count, setCount ] = useState(0);
+class UglyApp extends React.Component {
+  state = {
+    number:0
+  };
 
-  const onChangeCount = (event) => {
+  onChangeCount = (event) => {
     const { target: { name } } = event;
     if(name === "increment"){
-      setCount(preCount => preCount + 1);
-    }else if (name === "decrement"){
-      setCount(preCount => preCount -1);
+      this.setState(pre => ({number:pre.number +1}));
+    }else if(name ==="decrement"){
+      this.setState(pre => ({number: pre.number -1}));
     }
   };
 
-  return (
-    <div className="App">
-      <span>{count}</span>
-      <button onClick={onChangeCount} name="increment">increment</button>
-      <button onClick={onChangeCount} name="decrement">decrement</button>
-    </div>
-  );
+  render(){
+    const { number } = this.state;
+    return (
+      <div className="App">
+        <span>{number}</span>
+        <button onClick={this.onChangeCount} name="increment">increment</button>
+        <button onClick={this.onChangeCount} name="decrement">decrement</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+
+export default UglyApp;
